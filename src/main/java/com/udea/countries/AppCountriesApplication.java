9f4b4c52cbc4ba0f2a7b8a18749cad5fad7328ad
jairleal157 <jair.leal@udea.edu.co>
2023-10-08 14:25:31 -0500

    inicializacion proyecto

---
+++ src/main/java/com/udea/countries/AppCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.udea.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCountriesApplication.class, args);
+	}
+
+}
